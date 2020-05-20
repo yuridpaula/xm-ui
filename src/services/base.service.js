8f27815342e09586f5504867c2d91735cc2f023b
Yuri de Paula <yuri.oliveira@democrata.com.br>
2020-05-20 07:51:46 -0300

    alterações pendentes

--- src/services/base.service.js
+++ src/services/base.service.js
@@ -7,9 +7,9 @@ export let ApiBase = axios.create({
     }
 })
 
-ApiBase.interceptors.request.use( async (config) => {
+ApiBase.interceptors.request.use(async (config) => {
     //const token = 'Bearer eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJ5dXJpb2RwQGdtYWlsLmNvbSIsInJvbGUiOiJST0xFX1VTVUFSSU8iLCJjcmVhdGVkIjoxNTU4MTE1NTQxNTUzLCJleHAiOjIxNjI5MTU1NDF9.2hlCA5O2fPN5V8mAj1gz5xKaUUGgJ5RQ6tVe44bmq0ZL_uPxko2_eSbnZpzz8K875o3Jh7LiC0bHGzDfXVcMig'
-    
+
     const token = localStorage.getItem("token")
     if (token) {
         config.headers.Authorization = 'Bearer ' + token
@@ -17,7 +17,7 @@ ApiBase.interceptors.request.use( async (config) => {
         config.headers.Authorization = null
     }
 
-    
+
     return config
 })
 
